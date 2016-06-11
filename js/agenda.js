ae25fcea24de75aca2a09254d3a64db77af2c8ba
Anne <popanamaria40@gmail.com>
2016-06-11 11:15:49 +0300

    prima versiune de agenda2

--- js/agenda.js
+++ js/agenda.js
@@ -1,5 +1,20 @@
 console.debug('loading agenda');
 
-var row = '<tr><td>Mic</td><td>Francesca</td><td>075211225</td></tr>';
+function getRow(name) {
+    var row = '<tr><td>' + name + '</td><td>Francesca</td><td>075211225</td></tr>';
 
-$('#agenda tbody').append(row);
+}
+
+var contacts = [
+    'Matei',
+    'Andrei',
+    'Sebi',
+    'Oana',
+    'Ema',
+    'Manu'
+
+];
+
+for (var i = 0; i  <contacts; i++) {
+    $('#agenda tbody').append(getRow(contacts[i]));
+}
