2bba800b3dcd971ba9dee843676643acfbea1c0d
Anne <popanamaria40@gmail.com>
2016-06-11 11:39:35 +0300

    show ccontact from array

--- js/agenda.js
+++ js/agenda.js
@@ -1,19 +1,20 @@
 console.debug('loading agenda');
 
 function getRow(person) {
-    var firstName = person[0];
-    var lastName = person[1];
+    var firstName = person.firstName ;
+    var lastName = person.lastName ;
     var row = '<tr><td>' + firstName + '</td><td>' + lastName + '</td><td>075211225</td></tr>';
     return row;
 }
 
 var contacts = [
-    ['Vlad','Pop'],
-    ['Matei','Nicolae'],
-    ['Andrei' ,'XX' ],
-    ['Sebi', 'YY' ],
-    ['Gina', 'ZZ' ]
 
+    {firstName: 'Vlad', lastName: 'Pop'},
+    {firstName: 'Simona', lastName: 'Pop'},
+    {firstName: 'Ionela', lastName: 'Pop'},
+    {firstName: 'Gina', lastName: 'Pop'},
+    {firstName: 'Andra', lastName: 'Pop'},
+    {firstName: 'Ema', lastName: 'Pop'},
 
 ];
 
