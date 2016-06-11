41e32c3db80ca613ca331e1e3f6967341b5ae031
Anne <popanamaria40@gmail.com>
2016-06-11 11:28:17 +0300

    show ccontact from array

--- js/agenda.js
+++ js/agenda.js
@@ -1,20 +1,22 @@
 console.debug('loading agenda');
 
-function getRow(name) {
-    var row = '<tr><td>' + name + '</td><td>Francesca</td><td>075211225</td></tr>';
+function getRow(person) {
+    var firstName = person[0];
+    var lastName = person[1];
+    var row = '<tr><td>' + firstName + '</td><td>' + lastName + '</td><td>075211225</td></tr>';
 
 }
 
 var contacts = [
-    'Matei',
-    'Andrei',
-    'Sebi',
-    'Oana',
-    'Ema',
-    'Manu'
+    ['Nicolae','Matei'],
+    ['Pop','Matei'],
+    ['Andrei' ,'x' ],
+    ['Sebi', 'y' ]
+
 
 ];
 
 for (var i = 0; i  <contacts; i++) {
-    $('#agenda tbody').append(getRow(contacts[i]));
+    var person = contact
+    $('#agenda tbody').append(getRow(person));
 }
